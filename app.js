a0d8c68c60d7f032d4f4efca23adef2e5e246f15
robert.waggott <rob@earthware.co.uk>
2015-09-04 10:32:42 +0100

    creating a hello world in express

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require("express");
-var stylus = require("stylus");
+var morgan = require("morgan");
 var nib = require("nib");
+var stylus = require("stylus");
 
 var app = express();
 
 app.set("views", "./views");
 app.set("view engine", "jade");
-app.use(express.logger("dev"));
+app.use(express.static("./public"));
+
+app.use(morgan("combined"));
+
+app.get("/", function(req, res) {
+  res.end("Hello world");
+});
+
+app.listen(3000);
