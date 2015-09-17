634ad6a1ec0bc9264ebef3ede001fef27b87e014
robert.waggott <rob@earthware.co.uk>
2015-09-17 20:08:27 +0100

    adding in loading dialog

--- public/js/map.js
+++ public/js/map.js
@@ -4,9 +4,9 @@ $(function() {
 	var map = L.mapbox.map('map', 'robert-waggott.nf8f91b1')
 	    .setView([53.959965, -1.087298], 10);
 
+    NProgress.start();
+
 	$.getJSON("/geojson/Adur/2015-08-01").done(function(geojson) {
-		// see http://leafletjs.com/reference.html#featuregroup.
-		// and http://stackoverflow.com/questions/17277686/leaflet-js-center-the-map-on-a-group-of-markers
 		var layer = L.geoJson(geojson, {
 			style: {
 		    	"color": "#ff7800",
@@ -16,9 +16,11 @@ $(function() {
 			onEachFeature: function(feature, layer) {
 			    layer.on({
         			click: function() {
-        				// TODO: Open link to https://eservices.landregistry.gov.uk/www/wps/portal/!ut/p/b1/hc5BC4JAEAXgnzSzM-O6Hu3gugRJGJRziT1IGLpeot9fdqso5_bge48Bhc4UBQoJUQ4n0BTvwyXehjnFcclqz0xOjBGqHdscQ1Z5NoeMUfgJuj_Am7V-2yc4gn6wpiox8KbZet4Tov0C7zuOVsDyxwvgjysRdvU89TDp6NpwlQclu3xj/dl4/d5/L0lDU0lKSmdrS0NsRUpDZ3BSQ2dwUkNTQS9ZSVVJQUFJSUlJTU1JS0VFQUFDR09HT0NHSUJKRkpGQkpORE5EQk5ISUVBTExBISEvNEczYUQyZ2p2eWhDa3lGTU5RaWt5RktOUmprS2NhZ21Rb2dnL1o3XzMyODQxMTQySDgzNjcwSTVGRzMxVDUzOFY0LzAvaWJtLmludi8zMDM3NTQzMzk5MjYvc3BmX0FjdGlvbk5hbWUvc3BmX0FjdGlvbkxpc3RlbmVyL3NwZl9zdHJ1dHNBY3Rpb24vITJmTHJJbnNwaXJlSWRJbml0LmRv/
-        				// in the sidebar. 
-        				console.log(feature);
+        				$("#polygon_information_explanation").hide();
+
+        				$("#inspire_id").html("<a target='_blank' href='https://eservices.landregistry.gov.uk/www/wps/portal/!ut/p/b1/04_SjzS0tDQwMTIxMjLXj9CPykssy0xPLMnMz0vMAfGjzOKNjSxMDA1NjDwsjM3MDTxN3dyNDUNMjQ1MjPWDU_P0c6McFQH3SLFU/'>" + feature.properties.id + "</a>");
+        				$("#cadastral_ref").html(feature.properties.cadastralReference);
+        				$("#valid_from").html(feature.properties.validFrom);
         			}
 				});
 			}
@@ -27,5 +29,7 @@ $(function() {
 		layer.addTo(map);
 
 	 	map.fitBounds(layer.getBounds());
+
+	 	NProgress.done();
 	});
 });
